6204cd008f4446e24730a8fa9ee0d67df346b262
Tiobel <h.vieira.gomes@gmail.com>
2016-12-03 13:12:07 +0100

    Add files via upload

---
+++ PPT_Pointer_Code_1.ino
@@ -0,0 +1,25 @@
+#include <IRremote.h>
+
+int RECV_PIN = 2;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+int key_pressed;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    
+    key_pressed = results.value;
+    Serial.println(key_pressed);
+    
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
